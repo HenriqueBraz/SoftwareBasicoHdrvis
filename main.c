05813cbe2e95cb54bcd81746185e3eaa567b361c
Unknown <henrique.braz@acad.pucrs.br>
2018-11-05 22:25:17 -0200

    Falta implementar o resto  do ítem 2.4 - iniciado

---
+++ main.c
@@ -0,0 +1,171 @@
+#include <math.h>
+#include <string.h>		// para usar strings
+
+// Rotinas para acesso da OpenGL
+#include "opengl.h"
+
+// Rotinas para leitura de arquivos .hdr
+#include "rgbe.h"
+
+// Variáveis globais a serem utilizadas:
+
+// Dimensões da imagem de entrada
+int sizeX, sizeY;
+
+// Imagem de entrada
+RGBf* image;
+
+// Imagem de saída
+RGB8* image8;
+
+// Fator de exposição inicial
+float exposure = 1.0;
+
+// Modo de exibição atual
+int modo;
+
+// Função pow mais eficiente (cerca de 7x mais rápida)
+float fastpow(float a, float b);
+float fastpow(float a, float b) {
+     union { float f; int i; }
+      u = { a };
+      u.i = (int)(b * (u.i - 1065307417) + 1065307417);
+      return u.f;
+}
+
+// Função principal de processamento: ela deve chamar outras funções
+// quando for necessário (ex: algoritmos de tone mapping, etc)
+void process()
+{
+    printf("Exposure: %.3f\n", exposure);
+    //
+    // EXEMPLO: preenche a imagem com pixels cor de laranja...
+    //
+    //
+    // SUBSTITUA este código pelos algoritmos a serem implementados
+    //
+    int pos;
+    for(pos=0; pos<sizeX*sizeY; pos++) {
+        image8[pos].r = (unsigned char) (255 * exposure);
+        image8[pos].g = (unsigned char) (127 * exposure);
+        image8[pos].b = (unsigned char) (0 * exposure);
+    }
+
+//------------------------------------------------------------------------------
+// 2.1: código para LER a imagem de entrada (lendo a imagem tree.hdr): --->OK
+
+typedef struct {
+   float r, g, b;
+} RGBf;
+
+// Largura e altura da imagem
+int width, height;
+
+// Ponteiro para o início da imagem na memória
+RGBf* image;
+
+// Abre o arquivo
+FILE* arq;
+arq = fopen("tree.hdr","rb");
+
+// Lê o header do arquivo, de onde são extraídas a largura e altura
+RGBE_ReadHeader(arq, &width, &height, NULL);
+
+// Aloca memória para a imagem inteira
+image = (RGBf*) malloc(sizeof(RGBf) * width * height);
+
+// Finalmente, lê a imagem para a memória
+int result = RGBE_ReadPixels_RLE(arq, (float*)image, width, height);
+if (result == RGBE_RETURN_FAILURE) {
+   /// Tratamento de erro...
+}
+fclose(arq);
+//----------------------------------------------------------------------------------
+//2.2: Aplicação do fator de exposição ---> OK
+int comp = width * height;
+printf("%d", comp);
+
+
+    for(int i=0; i<comp; i++) {
+        image[i].r = (unsigned char) (image[i].r * exposure);
+        image[i].g = (unsigned char) (image[i].g * exposure);
+        image[i].b = (unsigned char) (image[i].b * exposure);
+    }
+
+//-----------------------------------------------------------------------------------
+
+ //2.3.1: Tone mapping por escala ---> ok
+
+    for(int i=0; i<comp; i++) {
+        image[i].r = (unsigned char) (image[i].r / (image[i].r + 0.5));
+        image[i].g = (unsigned char) (image[i].g / (image[i].g + 0.5));
+        image[i].b = (unsigned char) (image[i].b / (image[i].b + 0.5));
+    }
+ //------------------------------------------------------------------------------------
+    //2.3.2: Tone mapping por Correção gama ---> ok
+
+        for(int i=0; i<comp; i++) {
+        image[i].r = (unsigned char) (fastpow(image[i].r,2.0));
+        image[i].g = (unsigned char) (fastpow(image[i].g,2.0));
+        image[i].b = (unsigned char) (fastpow(image[i].b,2.0));
+    }
+
+//---------------------------------------------------------------------------------------
+
+   //2.4: Conversão para 24 bits
+
+        for(int i=0; i<comp; i++) {
+        image8[i].r = (unsigned char) (min(1.0,image[i].r) * 255);
+        image8[i].g = (unsigned char) (min(1.0,image[i].g) * 255);
+        image8[i].b = (unsigned char) (min(1.0,image[i].b) * 255);
+    }
+
+
+
+    //
+    // NÃO ALTERAR A PARTIR DAQUI!!!!
+    //
+    buildTex();
+}
+
+int main(int argc, char** argv)
+{
+    if(argc==1) {
+        printf("hdrvis [tree.hdr]\n");
+        exit(1);
+    }
+
+    // Inicialização da janela gráfica
+    init(argc,argv);
+
+
+
+
+    // Siga as orientações no enunciado para:
+    //
+    // 1. Descobrir o tamanho da imagem (ler header)
+    // 2. Ler os pixels
+    //
+
+    // TESTE: cria uma imagem de 800x600
+    sizeX = 800;
+    sizeY = 600;
+
+    printf("%d x %d\n", sizeX, sizeY);
+
+    // Aloca imagem float
+    image = (RGBf *)malloc(sizeof(RGBf) * sizeX * sizeY);
+
+    // Aloca memória para imagem de 24 bits
+    image8 = (RGB8*) malloc(sizeof(RGB8) * sizeX * sizeY);
+
+    exposure = 1.0f; // exposição inicial
+
+    // Aplica processamento inicial
+    process();
+
+    // Não retorna... a partir daqui, interação via teclado e mouse apenas, na janela gráfica
+    glutMainLoop();
+    return 0;
+}
+
